d697b8db9cd069e004b3aab71114b4bc8b04e439
demonvenerable347-tech <demonvenerable347@gmail.com>
2026-03-12 21:53:36 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -41,8 +41,6 @@ module.exports.languages = {
     }
 };
 
-// 🔹 এখানে আপনার ফটো Imgur লিংক করে বসাবেন ✅
-const helpImages = [
     "https://i.imgur.com/sxSn1K3.jpeg",
     "https://i.imgur.com/8WvpgUL.jpeg",
     "https://i.imgur.com/8WvpgUL.jpeg",
@@ -143,7 +141,7 @@ module.exports.run = function ({ api, event, args, getText }) {
 ${msg}
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: ${prefix}
-┃ 🤖 Bot Name: ${global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"}
+┃ 🤖 Bot Name: ${global.config.BOTNAME || "Bo Qing"}
 ┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
 ╰━━━━━━━━━━━━━━━━╯`;
 
